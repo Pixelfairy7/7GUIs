c2adda41057eae867f54e6f0560fb36d52576564
Erika Myles <erikamyles314@gmail.com>
2020-06-29 18:21:06 -0400

    Added counter project, styled

---
+++ src/components/counter.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export class counter extends Component {
+    constructor(props){
+        super(props);
+        this.incrementCounter = this.incrementCounter.bind('this');
+    }
+    state={
+        count: 0,
+    }
+   
+
+    incrementCounter =()=>{
+        this.setState({count: this.state.count +1})
+    }
+    render() {
+        return (
+            <div className="counter-container">
+            <h2>Counter</h2>
+                <div className="counter">
+                    <span className="counter-display">{this.state.count}</span>
+                    <button className="btn counter-btn" onClick={this.incrementCounter}>+</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default counter
